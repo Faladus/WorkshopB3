e2c236a4522903236fb672c7ed16ba35f7654dbc
Robsing <robin.balmadier@gmail.com>
2018-10-02 14:34:55 +0200

    MAJ Mardi 14:34

--- WorkshopB2/app/src/main/java/com/epsi/wkshop/workshopb2/EcolesActivity.java
+++ WorkshopB2/app/src/main/java/com/epsi/wkshop/workshopb2/EcolesActivity.java
@@ -2,6 +2,10 @@ package com.epsi.wkshop.workshopb2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class EcolesActivity extends AppCompatActivity {
 
@@ -9,5 +13,53 @@ public class EcolesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecoles); //test
+    }
+
+    protected void showEpsi(View v){
+        TextView txt = findViewById(R.id.epsiText);
+        if(txt.getHeight() ==0)
+            txt.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        else
+            txt.setLayoutParams(new LinearLayout.LayoutParams(0, 0));
+    }
+
+    protected void showWis(View v){
+        TextView txt = findViewById(R.id.wisText);
+        if(txt.getHeight() ==0)
+            txt.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        else
+            txt.setLayoutParams(new LinearLayout.LayoutParams(0, 0));
+    }
+
+    protected void showIpi(View v){
+        TextView txt = findViewById(R.id.ipiText);
+        if(txt.getHeight() ==0)
+            txt.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        else
+            txt.setLayoutParams(new LinearLayout.LayoutParams(0, 0));
+    }
+
+    protected void showTroisa(View v){
+        TextView txt = findViewById(R.id.troisaText);
+        if(txt.getHeight() ==0)
+            txt.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        else
+            txt.setLayoutParams(new LinearLayout.LayoutParams(0, 0));
+    }
+
+    protected void showCbio(View v){
+        TextView txt = findViewById(R.id.cbioText);
+        if(txt.getHeight() ==0)
+            txt.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        else
+            txt.setLayoutParams(new LinearLayout.LayoutParams(0, 0));
+    }
+
+    protected void showCefam(View v){
+        TextView txt = findViewById(R.id.cefamText);
+        if(txt.getHeight() ==0)
+            txt.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        else
+            txt.setLayoutParams(new LinearLayout.LayoutParams(0, 0));
     }
 }
--- WorkshopB2/app/src/main/java/com/epsi/wkshop/workshopb2/MainActivity.java
+++ WorkshopB2/app/src/main/java/com/epsi/wkshop/workshopb2/MainActivity.java
@@ -2,12 +2,30 @@ package com.epsi.wkshop.workshopb2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    /**Bouton Ecoles et formations    */
+    Button btn_Ecoles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); 
+        setContentView(R.layout.activity_main);
+
+        btn_Ecoles = findViewById(R.id.button2);
+
+
+        btn_Ecoles.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //démarrage activité Ecoles
+                startActivity(new Intent(MainActivity.this, EcolesActivity.class));
+            }
+        });
     }
 }
